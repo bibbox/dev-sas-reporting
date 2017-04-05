847e0f77c24f0cb3480eb2edd61f09fb35fa8019
hofinho <ph.hofer@live.at>
2017-04-05 16:36:37 +0200

    Add files via upload

---
+++ src/main/java/sasreporting/redcap/REDCapSPARKService.java
@@ -0,0 +1,267 @@
+package msig.toolbox.redcap.spark;
+
+import static spark.Spark.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.mail.EmailException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import msig.toolbox.redcap.con.REDCapHttpConnector;
+import msig.toolbox.redcap.jasper.REDCapJRXMLTemplateCompiler;
+import msig.toolbox.redcap.mail.REDCapMailClient;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+
+public class REDCapSPARKService {
+
+	
+	private static final Logger logger = LogManager.getLogger(REDCapSPARKService.class);
+	
+	public static String PROPERTIES_FILE_MAIL = "mail.properties";
+	public static String PROPERTIES_FILE_REPORT_ALERT = "service.properties";
+	
+	public static String MAIL_TEXT_PATH = "mail.txt";
+	
+	private static final String FIELD_PROJECT = "project_id";
+	private static final String FIELD_RECORD = "record";
+	
+	public static final String FIELD_PROJECTS_TEMPLATE = "template";
+	
+	public static final String FIELD_SERVICE_PROJECTS = "project_file";
+	public static final String FIELD_SERVICE_TEMPLATE_DIR = "template_dir";
+	public static final String FIELD_SERVICE_MAIL = "mail_field";
+	public static final String FIELD_SERVICE_CC = "cc_field";
+
+	public static final String REDCAP_FIELD_URL = "redcap_url";
+	
+	private static Properties mailConf, serviceConf;
+	
+	
+	public REDCapSPARKService() {
+		
+		//BasicConfigurator.configure();
+		
+		//staticFileLocation("/public");
+	}
+	
+	/*
+	 * Initializes the REDCap report tool SPARK service
+	 */
+	
+	public void init() {
+		
+		logger.info("Starting REDCap listener");
+		
+		try {
+			
+			mailConf = new Properties();
+			mailConf.load(new FileReader(REDCapSPARKService.PROPERTIES_FILE_MAIL));
+			
+			serviceConf = new Properties();
+			serviceConf.load(new FileReader(REDCapSPARKService.PROPERTIES_FILE_REPORT_ALERT));
+			
+			before((request, response) -> {
+			    
+				Map<String, String[]> queryMap = request.queryMap().toMap();
+				
+				String url = serviceConf.getProperty(REDCapHttpConnector.FIELD_URL); 
+				
+				logger.info(url + " not a valid service");
+				
+				boolean authenticated = (queryMap.get(REDCapSPARKService.REDCAP_FIELD_URL) != null && url.startsWith(queryMap.get(REDCapSPARKService.REDCAP_FIELD_URL)[0]));
+				
+			    if (!authenticated) {
+			    	
+			    	halt(401, "Invalid REDCap service");
+			    }
+			});
+			
+			post("/redcap_trigger_receive/", (request, response) -> {
+				
+				logger.info("Incoming event:\n");
+			
+				Map<String, String[]> queryMap = request.queryMap().toMap();
+				
+				String url = serviceConf.getProperty(REDCapHttpConnector.FIELD_URL); 
+				
+				logger.info("REDCap Url: " + queryMap.get(REDCapSPARKService.REDCAP_FIELD_URL)[0]);
+				
+				String[] projectIDs = queryMap.get(REDCapSPARKService.FIELD_PROJECT);
+				String[] recordIDs = queryMap.get(REDCapSPARKService.FIELD_RECORD);
+				
+				for(int i=0; i<recordIDs.length; i++) {
+					
+					String[] projectTokenAndTemplate = getProjectTokenandTemplate(projectIDs[i]);
+					
+					REDCapHttpConnector httpCon = new REDCapHttpConnector(url, projectTokenAndTemplate[0]); 
+					
+					String mailFields = serviceConf.getProperty(REDCapSPARKService.FIELD_SERVICE_MAIL)+","+serviceConf.getProperty(REDCapSPARKService.FIELD_SERVICE_CC);
+					
+					InputStream mailInfo = httpCon.getRecordsFromREDCapProject(recordIDs[i], mailFields, "csv");
+					
+					CSVParser csvpMailInfo = new CSVParser(new InputStreamReader(mailInfo), CSVFormat.DEFAULT.withHeader());
+					Map<String, Integer> csvpHeader = csvpMailInfo.getHeaderMap();
+					List<CSVRecord> mainInfRecords = csvpMailInfo.getRecords();
+					
+					csvpMailInfo.close();
+					mailInfo.close();
+					
+					if(mainInfRecords.size() > 0) {
+						
+						CSVRecord record = mainInfRecords.get(0);
+						String to = record.get(csvpHeader.get(serviceConf.getProperty(REDCapSPARKService.FIELD_SERVICE_MAIL)));
+						
+						String ccEnabled = record.get(csvpHeader.get(serviceConf.getProperty(REDCapSPARKService.FIELD_SERVICE_CC)));
+						
+						logger.info("Sending email to: " + to + " CC enabled: " + ccEnabled);
+					
+						boolean enabled = false;
+						
+						if("Yes".equals(ccEnabled)) {
+							
+							enabled = true;
+						}
+								
+						if(to == null || "".equals(to)) {
+							
+							logger.info("No valid mail address");
+							halt(401, "No valid mail address");
+						}
+						
+						logger.info("Project ID: " + projectIDs[i] + ", Record ID: " + recordIDs[i]);
+						
+						String jrxmlTemplatePath = serviceConf.getProperty(REDCapSPARKService.FIELD_SERVICE_TEMPLATE_DIR)+File.separator+projectTokenAndTemplate[1];
+						
+						logger.info("JRXML template path: " + jrxmlTemplatePath);
+						
+						sendMail(httpCon, recordIDs[i], to, jrxmlTemplatePath, enabled);
+					}
+				}
+				return true;
+			});
+		
+		} catch (IOException e) {
+			
+			logger.error(e.getMessage());
+		}
+	}
+	
+	/**
+	 * (i) Generates a PDF report from a REDCap project record and a jrxml-based report template
+	 * (ii) Sends a generated PDF report to a mail recipient specified within the record 
+	 * 
+	 * @param httpCon HttpConnector instance
+	 * @param recordID REDCap record ID
+	 * @param to Email recipient
+	 * @param jrxmlTemplatePath JRXML template file path
+	 * @param ccEnabled True, if the report should be sent to a mail recipient
+	 * 
+	 * @throws EmailException If email could not be sent
+	 * @throws IOException If a resource file (mail.txt, template.jrxml) is missing or not accessible 
+	 * @throws JRException If the report could not be generated 
+	 * @throws UnsupportedOperationException If a record stream cannot be read
+	 *
+	 */
+	
+	public void sendMail(REDCapHttpConnector httpCon, String recordID, String to, String jrxmlTemplatePath, boolean ccEnabled) throws UnsupportedOperationException, IOException, JRException, EmailException {
+		
+		InputStream record = httpCon.getRecordsFromREDCapProject(recordID, "", "csv");
+		
+		FileInputStream jrxmlTemplate = new FileInputStream(jrxmlTemplatePath);
+		
+		REDCapJRXMLTemplateCompiler compiler = new REDCapJRXMLTemplateCompiler();
+		
+		JasperPrint jp = compiler.getReportFromJRXML(record, jrxmlTemplate);
+	
+		jrxmlTemplate.close();
+		
+		String reportName = FilenameUtils.removeExtension((new File(jrxmlTemplatePath).getName()));
+		
+		String cc = "";
+		
+		if(ccEnabled) {
+			
+			cc = mailConf.getProperty(REDCapMailClient.MAIL_CC);
+		}
+		
+		logger.info("CC Address: " + cc);
+		
+		REDCapMailClient mailClient = new REDCapMailClient();
+		
+		String sent = mailClient.sendMailWithREDCapRecordAttachement(to, cc, new ByteArrayInputStream(JasperExportManager.exportReportToPdf(jp)), reportName, "REDCAP Report Event Mail", readMailText());
+		
+		logger.info(sent);
+		
+		record.close();
+	}
+	
+	/**
+	 * 
+	 * @param projectID REDCap project ID
+	 * @return REDCap API token and associated jrxml template path
+	 * @throws FileNotFoundException If the project.csv file is missing 
+	 * @throws IOException If the csv input stream cannot be read
+	 */
+	
+	public String[] getProjectTokenandTemplate(String projectID) throws FileNotFoundException, IOException {
+		
+		CSVParser csvpProject = new CSVParser(new InputStreamReader(
+				new FileInputStream(serviceConf.getProperty(REDCapSPARKService.FIELD_SERVICE_PROJECTS))), CSVFormat.DEFAULT.withHeader());
+		
+		List<CSVRecord> projects = csvpProject.getRecords();
+		
+		Map<String, Integer> csvpHeader = csvpProject.getHeaderMap();
+		
+		csvpProject.close();
+		
+		for(CSVRecord project: projects) {
+			
+			if(projectID.equals(project.get(csvpHeader.get(REDCapSPARKService.FIELD_PROJECT)))) {
+				
+				String[] tokenAndTemplate = new String[2];
+				tokenAndTemplate[0] = project.get(csvpHeader.get(REDCapHttpConnector.FIELD_TOKEN));
+				tokenAndTemplate[1] = project.get(csvpHeader.get(REDCapSPARKService.FIELD_PROJECTS_TEMPLATE));
+				
+				return tokenAndTemplate;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * 
+	 * @return Mail text body
+	 * @throws IOException If the mail.txt file cannot be accessed
+	 */
+	
+	private String readMailText() throws IOException {
+		
+		File file = new File(REDCapSPARKService.MAIL_TEXT_PATH);
+		FileInputStream fis = new FileInputStream(file);
+		byte[] data = new byte[(int) file.length()];
+		fis.read(data);
+		fis.close();
+
+		String str = new String(data, "UTF-8");
+		
+		return str;
+	}
+}
---
+++ src/main/java/sasreporting/redcap/SPARKServiceMain.java
@@ -0,0 +1,98 @@
+package msig.toolbox.redcap.spark;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.URL;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+public class SPARKServiceMain {
+
+	
+	private static final Logger logger = LogManager.getLogger(SPARKServiceMain.class);
+	
+	private static String FIELD_ACTION = "action";
+	private static String FIELD_ACTION_START = "start";
+	private static String FIELD_ACTION_STOP = "stop";
+	private static String FIELD_TOKEN_SHUTDOWN = "shutdown_token";
+	private static String FIELD_PORT = "jetty_port";
+	
+	private static final Options options = new Options()
+			.addOption(FIELD_ACTION, true, "start, stop")
+			.addOption(FIELD_PORT,true,"Jetty port")
+			.addOption(FIELD_TOKEN_SHUTDOWN, true, "Stop token");
+	
+	/**
+	 * Start, Stop command of the REDCap SPARK service
+	 * @param argv single argument to start and stop the SPARK service
+	 */
+	
+	public static void main(String[] argv) {
+		
+		CommandLineParser parser = new DefaultParser();
+		
+		try {
+			
+			CommandLine cmd = parser.parse(options, argv);
+			
+			if(cmd.hasOption(FIELD_ACTION)) {
+				
+				if(FIELD_ACTION_START.equals(cmd.getOptionValue(FIELD_ACTION))) {
+					
+					REDCapSPARKService service = new REDCapSPARKService();
+					service.init();
+				}
+				
+				if(FIELD_ACTION_STOP.equals(cmd.getOptionValue(FIELD_ACTION))) {
+					
+					if(cmd.hasOption(FIELD_TOKEN_SHUTDOWN) && cmd.hasOption(FIELD_PORT)) {
+						
+						stopService(cmd.getOptionValue(FIELD_PORT), cmd.getOptionValue(FIELD_TOKEN_SHUTDOWN));
+					}
+					else {
+						
+						throw new Exception("Shutdown token or port is missing.");		
+					}
+				}
+			}
+		
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	/**
+	 * Stops the SPARK service
+	 */
+	
+	private static void stopService(String port, String shutdownToken) {
+		
+		try {
+			
+			Socket s = new Socket(InetAddress.getByName("127.0.0.1"), Integer.parseInt(port));
+		    
+			OutputStream out = s.getOutputStream();
+		    out.write((shutdownToken + "\r\nstop\r\n").getBytes());
+		    out.flush();
+		    
+		    s.close();
+		
+		} catch (IOException e) {
+			
+			logger.error(e);
+		}
+	}
+}
