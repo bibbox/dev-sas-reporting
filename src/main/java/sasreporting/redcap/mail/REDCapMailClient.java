aef80cb03aa7da88b15dd5eff3332cc374f99d10
hofinho <ph.hofer@live.at>
2017-04-09 09:51:00 +0200

    Update REDCapMailClient.java

--- src/main/java/sasreporting/redcap/mail/REDCapMailClient.java
+++ src/main/java/sasreporting/redcap/mail/REDCapMailClient.java
@@ -21,7 +21,7 @@ public class REDCapMailClient {
 
 	private static final Logger logger = LogManager.getLogger(REDCapMailClient.class);
 	
-	public static String PROPERTIES_FILE = "mail.properties";
+	public static String PROPERTIES_FILE = "/config/mail.properties";
 	public static String MAIL_SERVER = "mail_server";
 	public static String MAIL_SENDER = "mail_sender";
 	public static String MAIL_USER = "mail_user";
