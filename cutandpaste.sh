15e8113c2c37cbdb0e512c4b79a0180af7f5cf76
lomadi <lomadi@mac.com>
2017-04-09 11:24:59 +0200

    add some command for later "cut and paste"

---
+++ cutandpaste.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/bibbox/dev-sas-reporting.git
+
+git pull
+git add .
+git commit -m "update from tool server"
+git push -u origin master
+
+git remote add origin https://github.com/bibbox/dev-sas-reporting.git 
